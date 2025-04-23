29f76270eb356bd738dde9b97202c8635573759c
Israel Adewuyi <isistickz@gmail.com>
2025-04-23 05:50:36 +0300

    support for cuda

--- transformer_sampling/sampler.py
+++ transformer_sampling/sampler.py
@@ -12,11 +12,13 @@ class Transformer_Sampler:
         model (AutoModelForCausalLM): The pre-trained transformer model for text generation.
         config: The configuration of the model.
         tokenizer (AutoTokenizer): The tokenizer corresponding to the model.
+        device (str): The device used for text generation
     """
-    def __init__(self, model: AutoModelForCausalLM, tokenizer: AutoTokenizer) -> None:
+    def __init__(self, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, device: str) -> None:
         self.model = model
         self.config = model.config
         self.tokenizer = tokenizer
+        self.device = device
 
     @torch.inference_mode()
     def sample(self, prompt: str, max_new_tokens: int, **kwargs) -> str:
@@ -29,12 +31,13 @@ class Transformer_Sampler:
         Returns:
             str: The generated text including the prompt.
         """
-        input_toks = self.tokenizer(prompt, return_tensors="pt")["input_ids"]
+        input_toks = self.tokenizer(prompt, return_tensors="pt")["input_ids"].to(self.device)
         
         for _ in range(max_new_tokens):
             output = self.model(input_toks)
             logits = output.logits[0, -1, :]
-            new_token = torch.tensor([Transformer_Sampler.sample_next_token(logits, **kwargs)], dtype=torch.int64)
+            new_token = torch.tensor([Transformer_Sampler.sample_next_token(logits, **kwargs)], 
+                                     dtype=torch.int64, device=self.device)
             input_toks = torch.cat([input_toks, new_token[None,]], dim=-1)
 
             if new_token.item() == getattr(self.tokenizer, "eos_token_id", None):
@@ -78,11 +81,12 @@ class Transformer_Sampler:
 
 if __name__ == "__main__":
     model_path = "gpt2"
-    model = AutoModelForCausalLM.from_pretrained(model_path)
+    device = "cuda:3" if torch.cuda.is_available() else "cpu"
+    model = AutoModelForCausalLM.from_pretrained(model_path).to(device)
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     model.config.use_cache=False
     
-    transformer_sampler = Transformer_Sampler(model, tokenizer)
+    transformer_sampler = Transformer_Sampler(model, tokenizer, device)
 
     prompt = "The dog"
     print(transformer_sampler.sample(prompt, max_new_tokens=512, temperature=0.0))
