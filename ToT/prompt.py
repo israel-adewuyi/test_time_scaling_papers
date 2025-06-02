64c1e6b2b550634c1856ff91957e5cd340c267e2
Israel Adewuyi <isistickz@gmail.com>
2025-06-02 09:57:52 +0300

    made prompt clearer

--- ToT/prompt.py
+++ ToT/prompt.py
@@ -3,8 +3,10 @@ You are presented with some numbers.
 The goal is to combine them using mathematical operators like +, -, /, * and arrive at the number 24.
 Your task is to suggest {k} possible next steps.
 For each step, combine any two numbers in the original input with any of the given mathematical operations.
-Write out the operation as well as the numbers left.
-Examples are provided below, where 2 posible next steps are suggested. 
+For any step where you use division, only pick two numbers if the division operation gives an integer.
+Write out the operation as well as the numbers left. 
+Do not write anything asides the possible next steps as shown in the examples below.
+Examples are provided below, where 2 posible next steps are suggested. You are meant to follow the examples and print only next steps. 
 Example 
 Input: 
 1 1 4 6
@@ -29,6 +31,9 @@ VALUE_PROMPT_24 = """
 You are presented with some numbers.
 The goal is to combine them using mathematical operators like +, -, /, * and arrive at the number 24.
 Your task is to evaluate if the given numbers can reach 24 and your response should be one of the following {{sure, likely, impossible}}
+If it's impossible that the numbers you are given will be combined with the operations above to get 24, your answer should be impossible.
+If you're certain that there is a set of operations that can be used to combine the given numbers to arrive at 24, your answer should be sure
+If it's likely that some combinations give 24, your answer should be likely.
 The last line of your response should be Answer: answer
 
 Example 1
